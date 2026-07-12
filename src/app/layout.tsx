6796d8365b44c10ce9712ed7b10579673f56712d
Sani Yakubu <ysani095@gmail.com>
2026-07-12 13:52:43 +0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import QueryProvider from "./query-provider";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
